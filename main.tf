ba0e64c6e07a2ae9ad32b93cf47fe9fd4ac93563
vlads <vlads.carenoks@1nce.com>
2020-09-30 09:52:26 +0300

    Ability to add custom domain names (aws_api_gateway_base_path_mapping)

--- main.tf
+++ main.tf
@@ -13,3 +13,11 @@ resource "aws_api_gateway_deployment" "deployment" {
     create_before_destroy = false
   }
 }
+
+resource "aws_api_gateway_base_path_mapping" "ase_path_mapping" {
+  count       = var.custom_domain_name == null ? 0 : 1
+  stage_name  = aws_api_gateway_deployment.deployment.stage_name
+  domain_name = var.custom_domain_name
+  api_id      = var.rest_api_id
+  base_path   = var.base_path
+}
--- variables.tf
+++ variables.tf
@@ -11,3 +11,15 @@ variable "rest_api_id" {
   type        = string
   description = "aws_api_gateway id"
 }
+
+variable "custom_domain_name" {
+  type        = string
+  default     = null
+  description = "The already-registered domain name to connect the API to. Will create 'aws_api_gateway_base_path_mapping' if present and will do nothing if omitted"
+}
+
+variable "base_path" {
+  type        = string
+  default     = null
+  description = "Path segment that must be prepended to the path when accessing the API via this custom_domain_name"
+}
